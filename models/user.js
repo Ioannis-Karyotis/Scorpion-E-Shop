72c5e504328789b134f926254c44a9e908c955ac
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2020-04-12 17:00:50 +0300

    second master commit

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-	username: String,
-	email:    String,
-	password: String
+	username: { type:String, required:true },
+	email:    { type:String, required:true },
+	password:String
 });
 UserSchema.methods.validPassword = function( pwd ) {
     // EXAMPLE CODE!
