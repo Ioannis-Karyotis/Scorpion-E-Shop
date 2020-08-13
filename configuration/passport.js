98a1a2d69eecd87ba1bace8570a46256f3735855
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2020-08-13 23:57:43 +0300

    Configure mongoose

--- configuration/passport.js
+++ configuration/passport.js
@@ -102,7 +102,7 @@ passport.use('forgot_pass', new JwtStrategy({
 passport.use("facebook" , new FacebookStrategy({
       clientID: config.oauth.facebook.clientID,
       clientSecret: config.oauth.facebook.clientSecret,
-      callbackURL: "https://scorpion-store.herokuapp.com/auth/facebook/callback",
+      callbackURL: "http://localhost:3000/auth/facebook/callback",
       profileFields: ['id', 'emails', 'name' , 'picture.type(large)']
     },
     function(accessToken, refreshToken, profile, done) {
@@ -150,7 +150,7 @@ passport.use("facebook" , new FacebookStrategy({
 passport.use("google" , new GoogleStrategy({
       clientID: config.oauth.google.clientID,
       clientSecret: config.oauth.google.clientSecret,
-      callbackURL: "https://scorpion-store.herokuapp.com/auth/google/callback",
+      callbackURL: "http://localhost:3000/auth/google/callback",
       // profileFields: ['id', 'emails', 'name']
     },
     function(accessToken, refreshToken, profile, done) {
