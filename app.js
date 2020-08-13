4630e6d1b95e110488b21f99b6ffbc3de29df336
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2020-08-13 22:32:18 +0300

    Configure mongoose

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ const options = {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
   family: 4 // Use IPv4, skip trying IPv6
 };
 mongoose.connect(MONGODB_URI,options)
