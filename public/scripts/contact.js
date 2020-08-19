13b3f61103797d2a7e62cd30a6f02034bf3d6c56
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2020-08-20 01:22:59 +0300

    redirect page

---
+++ public/scripts/contact.js
@@ -0,0 +1,6 @@
+$(document).ready(function() { 
+	if (document.getElementById("success") != null || document.getElementById("error") != null) {
+		var elmnt = document.getElementById("contact-form");
+  		elmnt.scrollIntoView();
+	}
+});
