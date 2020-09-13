363d2aff06232ab37d66615875f624498bc2129f
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2020-09-13 14:37:48 +0300

    new heroku branch

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/Scorpion",{ useNewUrlParser: true, useUnifiedTopology:true  });
-seedDB(); //seed the database with products
+//seedDB(); //seed the database with products
 
 
 
