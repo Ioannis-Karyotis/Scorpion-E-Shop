b5556e9626b4e9e0a3b26abf47ba977057fc194c
Ioannis Karyotis <tejohny@arx.net>
2020-10-08 14:09:52 +0300

    Production 2

--- app.js
+++ app.js
@@ -36,13 +36,6 @@ const indexRoutes 	 = require("./routes/index"),
 
 dotenv.config();
 
-app.use(function(req, res, next) {
-	if (req.headers['x-forwarded-proto'] !== 'https' && req.path !== process.env.URL) {
-	  return res.redirect(['https://', req.get('Host'), req.url].join(''));
-	}
-	return next();
-});
-
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(bodyParser.json({
