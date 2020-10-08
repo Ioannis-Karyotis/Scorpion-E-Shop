ae1b7f7a7ca0120677f935dad97d8ba0fd1d0164
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2020-10-08 10:54:59 +0000

    lol

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ cron.schedule("0 */2 * * *", function() {
 
 if(process.env.ENV == "production") {
       // Certificate
-      const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-      const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-      const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+      const privateKey = fs.readFileSync('/etc/letsencrypt/live/scorpionclothing.gr/privkey.pem', 'utf8');
+      const certificate = fs.readFileSync('/etc/letsencrypt/live/scorpionclothing.gr/cert.pem', 'utf8');
+      const ca = fs.readFileSync('/etc/letsencrypt/live/scorpionclothing.gr/chain.pem', 'utf8');
 
       const credentials = {
             key: privateKey,
