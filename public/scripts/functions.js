bbddf1f69ffd559ff8f07d1003680efc4bacb218
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2020-11-23 11:13:37 +0200

    Loader

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,3 +1,15 @@
+$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( "#loadingDiv" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( "#loadingDiv" ).remove(); //makes page more lightweight 
+  });  
+}
+
+
 $(document).ready(function() {
 	// Gets the span width of the filled-ratings span
 	// this will be the same for each rating
