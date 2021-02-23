b6f65dbcd0126f6f13e944ecedee4da98a78de70
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2021-02-23 21:31:13 +0200

    Cookies policy

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,5 +7,21 @@ $(document).ready(function() {
             h: window.screen.height
         },
     });
-  
+    // window.cookieconsent.initialise({
+    //     "palette": {
+    //       "popup": {
+    //         "background": "#3c404d",
+    //         "text": "#d6d6d6"
+    //       },
+    //       "button": {
+    //         "background": "#65a07b"
+    //       }
+    //     },
+    //     "theme": "classic",
+    //     "content": {
+    //       "message": "Η Ιστοσελίδα scorpionclothing.gr χρησιμοποιεί cookies, έτσι ώστε να εξασφαλιστεί η βέλτιστη πιθανή εμπειρία χρήστη.",
+    //       "dismiss": "Το κατάλαβα",
+    //       "link": "Μάθε περισσότερα"
+    //     }
+    //   });
   }); 
