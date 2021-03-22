784bb86d572b862055ffbc96544a3a67cf2849d6
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2021-03-22 16:59:01 +0200

    test

--- configuration/passport.js
+++ configuration/passport.js
@@ -175,7 +175,6 @@ passport.use("google" , new GoogleStrategy({
       clientID: config.oauth.google.clientID,
       clientSecret: config.oauth.google.clientSecret,
       callbackURL: process.env.ROOT+"/auth/google/callback",
-      passReqToCallback: true,
       profileFields: ['id', 'emails', 'name']
     },
     function(accessToken, refreshToken, profile, done) {
