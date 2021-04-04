98024fd603e4ee38e021461357d9efee03496a90
Ioannis Karyotis <ioannis.karyotis16@gmail.com>
2021-04-04 17:30:12 +0300

    new layout

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -14,5 +14,18 @@ $(document).ready(function() {
             window.sessionStorage.setItem('x-api-key',request.getResponseHeader('x-api-key'));  
         },
     });
+
+    $('.carousel1').carousel({
+        interval : 2000
+    })
+    $('.carousel2').carousel({
+        interval : 4000
+    })
+    $('.carousel5').carousel({
+        interval : 2500
+    })
+    $('.carousel6').carousel({
+        interval : 3500
+    })
 }); 
 
